0b2f012b85d23c7f67b659c3429e009fb0229d83
huongtran2 <phuongtranth8888@gmail.com>
2022-09-05 14:52:17 +0700

    sxswd

--- src/namnhuan.java
+++ src/namnhuan.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
-
-public class ptb2 {
+public class namnhuan {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int year;
