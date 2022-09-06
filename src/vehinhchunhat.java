d0fabec4da5f4f767eecb257f4cf1653ff8f52f9
huongtran2 <phuongtranth8888@gmail.com>
2022-09-06 16:06:22 +0700

    vehinh

---
+++ src/vehinhchunhat.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class vehinhchunhat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        }
+    }
