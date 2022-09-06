6a143ffa33cd7515547868a575f6dd2335ce6fb7
huongtran2 <phuongtranth8888@gmail.com>
2022-09-06 09:19:59 +0700

    sxswd

---
+++ src/bt4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class bt4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    System.out.println("nhập $ =");
+    int a = scanner.nextInt();
+    int b=(a*23000);
+    System.out.println(b);
+
+    }
+}
