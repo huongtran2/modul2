8c2fe97a19f4eed60985bfb7a6a99464919242f2
huongtran2 <phuongtranth8888@gmail.com>
2022-09-07 15:22:54 +0700

    min

---
+++ src/timMintrongmang.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class timMintrongmang {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int Min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (Min >= arr[i]) {
+            }
+        }System.out.println(Min);
+    }
+}
